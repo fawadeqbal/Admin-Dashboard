5322206c6176f98daf0f3451e63ec6e6bbdd4c33
Fawad Iqbal <122690338+fawadeqbal@users.noreply.github.com>
2023-08-17 16:48:57 +0500

    Update SensorPage.js

--- src/pages/SensorPage.js
+++ src/pages/SensorPage.js
@@ -1,7 +1,7 @@
-import { Helmet } from 'react-helmet-async';
-import { filter } from 'lodash';
-import { sentenceCase } from 'change-case';
-import { useEffect, useState } from 'react';
+import { Helmet } from "react-helmet-async";
+import { filter } from "lodash";
+import { sentenceCase } from "change-case";
+import { useEffect, useState } from "react";
 // @mui
 import {
   Card,
@@ -21,24 +21,28 @@ import {
   IconButton,
   TableContainer,
   TablePagination,
-} from '@mui/material';
+} from "@mui/material";
 // components
-import Label from '../components/label';
-import Iconify from '../components/iconify';
-import Scrollbar from '../components/scrollbar';
-import Modal from '../modal/SensorModal'
+import Label from "../components/label";
+import Iconify from "../components/iconify";
+import Scrollbar from "../components/scrollbar";
+import SensorModal from "../modal/SensorModal";
+import EditSensorModal from "../modal/EditSensorModal";
 // sections
-import { SensorListHead, SensorListToolbar } from '../sections/@dashboard/sensor';
-import { getAllSensors } from '../service/api';
+import {
+  SensorListHead,
+  SensorListToolbar,
+} from "../sections/@dashboard/sensor";
+import { getAllSensors } from "../service/api";
 
 // mock
 
 // ----------------------------------------------------------------------
 
 const TABLE_HEAD = [
-  { id: 'sensorId', label: 'SensorId', alignRight: false },
-  { id: 'status', label: 'Status', alignRight: false },
-  { id: '' },
+  { id: "sensorId", label: "SensorId", alignRight: false },
+  { id: "status", label: "Status", alignRight: false },
+  { id: "" },
 ];
 
 // ----------------------------------------------------------------------
@@ -54,7 +58,7 @@ function descendingComparator(a, b, orderBy) {
 }
 
 function getComparator(order, orderBy) {
-  return order === 'desc'
+  return order === "desc"
     ? (a, b) => descendingComparator(a, b, orderBy)
     : (a, b) => -descendingComparator(a, b, orderBy);
 }
@@ -67,7 +71,11 @@ function applySortFilter(array, comparator, query) {
     return a[1] - b[1];
   });
   if (query) {
-    return filter(array, (_sensor) => _sensor.sensorId.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+    return filter(
+      array,
+      (_sensor) =>
+        _sensor.sensorId.toLowerCase().indexOf(query.toLowerCase()) !== -1
+    );
   }
   return stabilizedThis.map((el) => el[0]);
 }
@@ -75,20 +83,31 @@ function applySortFilter(array, comparator, query) {
 export default function SensorPage() {
   const [sensorList, setSensorList] = useState([]);
   const [open, setOpen] = useState(null);
-  const [selectedSensor,setSelectedSensor]=useState({})
+  const [selectedSensor, setSelectedSensor] = useState({
+    location: {
+      type: 'Point',
+      coordinates: [0, 0],
+    },
+    sensorId: '',
+    status: 'inactive', // Default status
+    // Add other sensor properties here
+  });
   const [page, setPage] = useState(0);
 
-  const [order, setOrder] = useState('asc');
+  const [order, setOrder] = useState("asc");
 
   const [selected, setSelected] = useState([]);
 
-  const [orderBy, setOrderBy] = useState('sensorId');
+  const [orderBy, setOrderBy] = useState("sensorId");
 
-  const [filterName, setFilterName] = useState('');
+  const [filterName, setFilterName] = useState("");
 
   const [rowsPerPage, setRowsPerPage] = useState(5);
 
   const [openCreateModal, setOpenCreateModal] = useState(false);
+
+  // Add this state variable at the beginning of the component
+  const [openEditModal, setOpenEditModal] = useState(false);
 
   useEffect(() => {
     fetchSensorData();
@@ -100,9 +119,8 @@ export default function SensorPage() {
     setSensorList(data);
   }
 
-
-  const handleOpenMenu = (event,sensor) => {
-    setSelectedSensor(sensor)
+  const handleOpenMenu = (event, sensor) => {
+    setSelectedSensor(sensor);
     setOpen(event.currentTarget);
   };
 
@@ -111,8 +129,8 @@ export default function SensorPage() {
   };
 
   const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === 'asc';
-    setOrder(isAsc ? 'desc' : 'asc');
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
     setOrderBy(property);
   };
 
@@ -129,21 +147,25 @@ export default function SensorPage() {
   };
   const handleDeleteMutiple = () => {
     // Filter out the selected sensors from the sensorList
-    const updatedList = sensorList.filter(sensor => !selected.includes(sensor.sensorId));
-    
+    const updatedList = sensorList.filter(
+      (sensor) => !selected.includes(sensor.sensorId)
+    );
+
     // Clear the selected sensors
     setSelected([]);
-    
+
     // Update the sensorList state with the updated list
     setSensorList(updatedList);
   };
-  
+
   const handleDelete = () => {
-    const updatedList = sensorList.filter((sensor) => sensor._id !== selectedSensor._id);
+    const updatedList = sensorList.filter(
+      (sensor) => sensor._id !== selectedSensor._id
+    );
     setSensorList(updatedList);
     setOpen(null); // Close the popover when deleting
   };
-  
+
   const handleClick = (event, sensorId) => {
     const selectedIndex = selected.indexOf(sensorId);
     let newSelected = [];
@@ -154,7 +176,10 @@ export default function SensorPage() {
     } else if (selectedIndex === selected.length - 1) {
       newSelected = newSelected.concat(selected.slice(0, -1));
     } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(selected.slice(0, selectedIndex), selected.slice(selectedIndex + 1));
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
     }
     setSelected(newSelected);
   };
@@ -173,9 +198,14 @@ export default function SensorPage() {
     setFilterName(event.target.value);
   };
 
-  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - sensorList.length) : 0;
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - sensorList.length) : 0;
 
-  const filteredSensors = applySortFilter(sensorList, getComparator(order, orderBy), filterName);
+  const filteredSensors = applySortFilter(
+    sensorList,
+    getComparator(order, orderBy),
+    filterName
+  );
 
   const isNotFound = !filteredSensors.length && !filterName;
 
@@ -186,17 +216,31 @@ export default function SensorPage() {
       </Helmet>
 
       <Container>
-        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+          mb={5}
+        >
           <Typography variant="h4" gutterBottom>
             Sensor
           </Typography>
-          <Button variant="contained" startIcon={<Iconify icon="eva:plus-fill" />} onClick={() => setOpenCreateModal(true)}>
-        New Sensor
-      </Button>
+          <Button
+            variant="contained"
+            startIcon={<Iconify icon="eva:plus-fill" />}
+            onClick={() => setOpenCreateModal(true)}
+          >
+            New Sensor
+          </Button>
         </Stack>
 
         <Card>
-          <SensorListToolbar numSelected={selected.length} filterName={filterName} onFilterName={handleFilterBySensorId} handleDeleteMutiple={handleDeleteMutiple}/>
+          <SensorListToolbar
+            numSelected={selected.length}
+            filterName={filterName}
+            onFilterName={handleFilterBySensorId}
+            handleDeleteMutiple={handleDeleteMutiple}
+          />
 
           <Scrollbar>
             <TableContainer sx={{ minWidth: 800 }}>
@@ -211,37 +255,62 @@ export default function SensorPage() {
                   onSelectAllClick={handleSelectAllClick}
                 />
                 <TableBody>
-                  {filteredSensors.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((sensor) => {
-                    const { sensorId, status } = sensor;
-                    const selectedSensor = selected.indexOf(sensorId) !== -1;
+                  {filteredSensors
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((sensor) => {
+                      const { sensorId, status } = sensor;
+                      const selectedSensor = selected.indexOf(sensorId) !== -1;
 
-                    return (
-                      <TableRow hover key={sensorId} tabIndex={-1} role="checkbox" selected={selectedSensor}>
-                        <TableCell padding="checkbox">
-                          <Checkbox checked={selectedSensor} onChange={(event) => handleClick(event, sensorId)} />
-                        </TableCell>
+                      return (
+                        <TableRow
+                          hover
+                          key={sensorId}
+                          tabIndex={-1}
+                          role="checkbox"
+                          selected={selectedSensor}
+                        >
+                          <TableCell padding="checkbox">
+                            <Checkbox
+                              checked={selectedSensor}
+                              onChange={(event) => handleClick(event, sensorId)}
+                            />
+                          </TableCell>
 
-                        <TableCell component="th" scope="row" padding="none">
-                          <Stack direction="row" alignItems="center" spacing={2}>
-                            <Avatar />
-                            <Typography variant="subtitle2" noWrap>
-                              {sensorId}
-                            </Typography>
-                          </Stack>
-                        </TableCell>
+                          <TableCell component="th" scope="row" padding="none">
+                            <Stack
+                              direction="row"
+                              alignItems="center"
+                              spacing={2}
+                            >
+                              <Avatar />
+                              <Typography variant="subtitle2" noWrap>
+                                {sensorId}
+                              </Typography>
+                            </Stack>
+                          </TableCell>
 
-                        <TableCell align="left">
-                          <Label color={(status === 'banned' && 'error') || 'success'}>{sentenceCase(status)}</Label>
-                        </TableCell>
+                          <TableCell align="left">
+                            <Label
+                              color={
+                                (status === "banned" && "error") || "success"
+                              }
+                            >
+                              {sentenceCase(status)}
+                            </Label>
+                          </TableCell>
 
-                        <TableCell align="right">
-                          <IconButton size="large" color="inherit" onClick={(e)=>handleOpenMenu(e,sensor)}>
-                            <Iconify icon={'eva:more-vertical-fill'} />
-                          </IconButton>
-                        </TableCell>
-                      </TableRow>
-                    );
-                  })}
+                          <TableCell align="right">
+                            <IconButton
+                              size="large"
+                              color="inherit"
+                              onClick={(e) => handleOpenMenu(e, sensor)}
+                            >
+                              <Iconify icon={"eva:more-vertical-fill"} />
+                            </IconButton>
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })}
                   {emptyRows > 0 && (
                     <TableRow style={{ height: 53 * emptyRows }}>
                       <TableCell colSpan={6} />
@@ -255,7 +324,7 @@ export default function SensorPage() {
                       <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
                         <Paper
                           sx={{
-                            textAlign: 'center',
+                            textAlign: "center",
                           }}
                         >
                           <Typography variant="h6" paragraph>
@@ -265,7 +334,8 @@ export default function SensorPage() {
                           <Typography variant="body2">
                             No results found for &nbsp;
                             <strong>&quot;{filterName}&quot;</strong>.
-                            <br /> Try checking for typos or using complete words.
+                            <br /> Try checking for typos or using complete
+                            words.
                           </Typography>
                         </Paper>
                       </TableCell>
@@ -292,31 +362,46 @@ export default function SensorPage() {
         open={Boolean(open)}
         anchorEl={open}
         onClose={handleCloseMenu}
-        anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        anchorOrigin={{ vertical: "top", horizontal: "left" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
         PaperProps={{
           sx: {
             p: 1,
             width: 140,
-            '& .MuiMenuItem-root': {
+            "& .MuiMenuItem-root": {
               px: 1,
-              typography: 'body2',
+              typography: "body2",
               borderRadius: 0.75,
             },
           },
         }}
       >
-        <MenuItem>
-          <Iconify icon={'eva:edit-fill'} sx={{ mr: 2 }} />
+        <MenuItem onClick={() => {
+          setOpenEditModal(true)
+          setOpen(null);
+        }}>
+          <Iconify
+            icon={"eva:edit-fill"}
+            sx={{ mr: 2 }}
+            
+          />
           Edit
         </MenuItem>
 
-        <MenuItem sx={{ color: 'error.main' }} onClick={handleDelete}>
-          <Iconify icon={'eva:trash-2-outline'} sx={{ mr: 2 }} />
+        <MenuItem sx={{ color: "error.main" }} onClick={handleDelete}>
+          <Iconify icon={"eva:trash-2-outline"} sx={{ mr: 2 }} />
           Delete
         </MenuItem>
       </Popover>
-      <Modal open={openCreateModal} onClose={() => setOpenCreateModal(false)} />
+      <SensorModal
+        open={openCreateModal}
+        onClose={() => setOpenCreateModal(false)}
+      />
+      <EditSensorModal
+        open={openEditModal}
+        onClose={() => setOpenEditModal(false)}
+        initialSensorData={selectedSensor} // Pass the selected sensor data for editing
+      />
     </>
   );
 }
