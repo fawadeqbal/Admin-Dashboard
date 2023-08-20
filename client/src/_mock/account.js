18613a96aeffd1cd51868408e5c84802bce64beb
Fawad Iqbal <122690338+fawadeqbal@users.noreply.github.com>
2023-08-20 20:55:52 +0500

    Update account.js

--- client/src/_mock/account.js
+++ client/src/_mock/account.js
@@ -3,7 +3,7 @@
 const account = {
   displayName: 'Fawad Iqbal',
   email: 'fawadeqbal@yahoo.com',
-  photoURL: '/assets/images/avatars/avatar_24.jpg',
+  photoURL: '/assets/images/avatars/avatar.jpg',
 };
 
 export default account;
