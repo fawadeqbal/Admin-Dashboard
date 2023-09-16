90df1e9a97bf565fad6c8a3a376fd87af810dc60
Fawad Iqbal <122690338+fawadeqbal@users.noreply.github.com>
2023-09-16 20:53:34 +0500

    bugs fixed

--- client/src/pages/DashboardAppPage.js
+++ client/src/pages/DashboardAppPage.js
@@ -11,7 +11,7 @@ import { getAllSensors } from '../service/api';
 import {
   AppCurrentVisits,
   AppWidgetSummary,
-  AppConversionRates,
+ 
 } from '../sections/@dashboard/app';
 
 // ----------------------------------------------------------------------
@@ -22,7 +22,7 @@ export default function DashboardAppPage() {
   const [totalSpaces, setTotalSpaces] = useState(0);
   const [availableSpaces, setAvailableSpaces] = useState(0);
   const [occupiedSpaces, setOccupiedSpaces] = useState(0);
-  const [bannedSpaces, setBannedSpaces] = useState(0);
+ 
 
   useEffect(() => {
     fetchSensorData(); // Assuming you have a function to fetch sensor data
@@ -32,10 +32,9 @@ export default function DashboardAppPage() {
     const data = await getAllSensors(); // Assuming you have a function to get sensor data
     const total = data.length;
     const available = data.filter(sensor => sensor.status === 'inactive'||sensor.status==='false').length;
-    const banned = data.filter(sensor => sensor.status==='banned').length;
-
-    const occupied = total - available - banned;
-    setBannedSpaces(banned)
+   
+    const occupied = total - available ;
+    
     setTotalSpaces(total);
     setAvailableSpaces(available);
     setOccupiedSpaces(occupied);
@@ -47,14 +46,14 @@ export default function DashboardAppPage() {
         <title> SpotTroop | Admin-Dashboard </title>
       </Helmet>
 
-      <Container maxWidth="xl">
+      <Container maxWidth="lg">
         <Typography variant="h4" sx={{ mb: 5 }}>
           Hi, Welcome back
         </Typography>
 
-        <Grid container spacing={3}>
+        <Grid container spacing={2}>
          
-          <Grid item xs={12} sm={6} md={3}>
+          <Grid item xs={16} sm={6} md={4}>
             <AppWidgetSummary
               title="Total Parking Spaces"
               total={totalSpaces}
@@ -62,7 +61,7 @@ export default function DashboardAppPage() {
             />
           </Grid>
           
-          <Grid item xs={12} sm={6} md={3}>
+          <Grid item xs={16} sm={6} md={4}>
             <AppWidgetSummary
               title="Use Parking Spaces"
               total={occupiedSpaces}
@@ -71,7 +70,7 @@ export default function DashboardAppPage() {
               
             />
           </Grid>
-          <Grid item xs={12} sm={6} md={3}>
+          <Grid item xs={16} sm={6} md={4}>
             <AppWidgetSummary
               title="Free Parking Spaces"
               total={availableSpaces}
@@ -82,7 +81,7 @@ export default function DashboardAppPage() {
          
 
          
-          <Grid item xs={12} md={6} lg={4}>
+          <Grid item xs={12} md={12} lg={12}>
             <AppCurrentVisits
               title="Parking Spaces"
               chartData={[
